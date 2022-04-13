fe85f4780ebfa346ca48c6626826064b12325886
Richievdheij <99058860@mydavinci.nl>
2022-04-13 14:46:06 +0200

    test.py
    Update

--- test.py
+++ test.py
@@ -1,81 +1,104 @@
-from curses import window
 import tkinter as tk
-from tkinter import ttk
+from tkinter import font
 from tkinter.messagebox import askyesno, askquestion
 
 pressButtons = ["Press W", "Press A", "Press S", "Press D", "Spacebar", "One-Click", "Double-Click", "Triple-Click"] # Buttons the user needs to press
-
-
+points = 0
 
 class App(tk.Tk):
     def __init__(window):
         super().__init__()
 
-        window.title('Simple FPS Trainer V1')
-        window.geometry('750x500')
+        window.configure(background='gray') # Window background color
+        window.title('Simple FPS Trainer V1') # Window title
+        window.geometry('750x500') # Window size
 
         # Launch button
-        launch_button = ttk.Button(
-            window, 
-            text='LAUNCH GAME', 
-            command=window.confirm
+        launch_button = tk.Button( # Makes the button
+            window,
+            text='LAUNCH GAME',
+            font=("arial",20,'bold'),
+            command=window.confirm # Makes the command pop-up window button
         )
+
         launch_button.pack(
+            fill='both',
             expand=True,
             padx=20,
-            pady=20
+            pady=20, 
         )
 
         # Launch label
-        launch_label = ttk.Label(
-            window, 
+        launch_label = tk.Label( # Makes the label
+            window,
+            background='gray',
             text='WELCOME TO SIMPLE FPS TRAINER V.1', 
-            font=(("arial",30, 'bold'))
+            font=("arial",20,'bold')
         )
+
         launch_label.pack(
-            fill='both',
             padx=0,
             pady=0
         )
+
     def confirm(window):
         answer = askyesno(title='Confirmation start game',
-                          message="Press 'Yes' to start the game. Timer will start after confirmation")
+                          message="Press 'Yes' to start the game.")
         if answer:
             window.destroy()
-            game()
+            startgame()
 
-def game():
+def startgame():
     window = tk.Tk() # Makes the window
     window.title("Simple FPS Trainer V1") # Window title
-    window.geometry("750x500")
+    window.geometry("750x500") # Window size
 
-    startButton1 = tk.Label(
+    startButton1 = tk.Label( # Makes the label
         window, 
         font=("arial",15,))
 
-    startButton1.configure(
-        text="Remaining time:")
+    startButton1.configure( # Adjust the label
+        text="Remaining time:") 
 
-    startButton1.pack(
+    startButton1.grid(
         fill='both',
-        pady=0,
-        padx=20
-    )
-
-    startButton2 = tk.Label(
-        window, 
-        font=("arial",15,))
-
-    startButton2.configure(
-        text="Start game")
-
-    startButton2.pack(
         pady=0,
         padx=0
     )
 
-def timer():
+    startButton2 = tk.Label( # Makes the label
+        window, 
+        font=("arial",15,))
+
+    startButton2.configure(
+        text="Points: {points}") 
+
+    startButton2.grid(
+        fill='both',
+        pady=0,
+        padx=0
+    )
+
+    startButton3 = tk.Button( # Makes the button
+        window, 
+        font=("arial",15,))
+
+    startButton3.configure(
+        text="Start game")
+
+    startButton3.grid(
+        expand=True,
+        pady=0,
+        padx=0,
+    )
+
+def game():
     return
+
+
+
+
+
 
 
 
